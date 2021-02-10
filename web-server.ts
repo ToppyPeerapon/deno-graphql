1ad698e66195b32f4e3e5fccbe4dd388c0140032
ToppyPeerapon <chun.peerapon@gmail.com>
2021-02-10 15:41:04 +0700

    config deno typescript and vscode extension

--- web-server.ts
+++ web-server.ts
@@ -6,7 +6,6 @@ console.log(`HTTP webserver running.  Access it at:  http://localhost:8080/`)
 for await (const request of server) {
   let bodyContent = "Your user-agent is:\n\n"
   bodyContent += request.headers.get("user-agent") || "Unknown"
-  return { notFound: true }
 
   request.respond({ status: 200, body: bodyContent })
 }
